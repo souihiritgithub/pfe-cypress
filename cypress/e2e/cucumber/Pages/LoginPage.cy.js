957a38a20d6ff99af49b9e5b78aa9690093ca477
amine mannai <amine.mannai@tessi.fr>
2024-02-19 11:16:04 +0100

    add xpath plugin to cypress project

--- cypress/e2e/cucumber/Pages/LoginPage.cy.js
+++ cypress/e2e/cucumber/Pages/LoginPage.cy.js
@@ -1,3 +1,4 @@
+import 'cypress-xpath';
 class LoginPage {
     enterURL() {
      cy.visit(
@@ -5,8 +6,8 @@ class LoginPage {
      );
    }
     enterUserNamePassword(username, password) {
-     cy.get('[id="form-username"]').type(username);
-     cy.get('[id="form-password"]').type(password);
+      cy.get('[id="form-username"]').should('be.visible').type(username);
+      cy.get('[id="form-password"]').should('be.visible').type(password);
    }
     clickSubmitButton() {
      cy.contains('Connexion').click();
@@ -15,9 +16,11 @@ class LoginPage {
      return cy.title().should("eq", "Owlink by Tessi");
    }
    verifyLoggedUserAndMenuSidebar() {
-    cy.get('#user-id').should('exist','be.visible').contains('amannai');
+    cy.get('#user-id').should('exist','be.visible').contains('ANE');
     cy.get('#left-panel').should('exist');
     cy.get('#logout').should('exist','be.visible');
+    // this is an example of how to implement xpath solution : it's an alternative for getting elements when there is no id !!!
+    cy.xpath('//*[@id="wid-id-0"]/header/h2').should('be.visible').contains('Rapport de travail');
    }
  }
  const login = new LoginPage();
--- cypress/e2e/cucumber/Tests/LoginTest.feature
+++ cypress/e2e/cucumber/Tests/LoginTest.feature
@@ -6,7 +6,7 @@ Feature: I want to login into the recette application with valid data
   Scenario: Login as a user
     When I entered valid credentials
       | identifiant  |mdp|
-      | amannai      |Owliance2024++|
+      | ANE      |OwlPWD01|
     And User click on sign in button
     Then Validate the title after login
     And  Validate existing elements in the redirected page
